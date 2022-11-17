f349f7b0d4869efd3ee1be04920d5af2cc410bfa
MrBurp <andreass.ziegler@gmail.com>
2022-11-17 18:10:59 +0100

    markdown working

--- script.js
+++ script.js
@@ -7,18 +7,21 @@ function addNewNote(text = "") {
   note.classList.add("note");
 
   note.innerHTML = `
-  <div class="tools">
-    <button class="edit"><i class="fas fa-edit"></i></button>
-    <button class="delete"><i class="fas fa-trash-alt"></i></button>
-  </div>
-  <div class="main" ${text ? "" : "hidden"}></div>
-  <textarea ${text ? "hidden" : ""}></textarea>
-  `;
+    <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+    </div>
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>
+    `;
 
-  const deleteBtn = note.querySelector(".delete");
   const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
   const main = note.querySelector(".main");
-  const textArea = note.querySelector(".textarea");
+  const textArea = note.querySelector("textarea");
+
+  textArea.value = text;
+  main.innerHTML = marked(text);
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
@@ -26,6 +29,13 @@ function addNewNote(text = "") {
 
   editBtn.addEventListener("click", () => {
     main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  textArea.addEventListener("input", (e) => {
+    const { value } = e.target;
+
+    main.innerHTML = marked(value);
   });
 
   document.body.appendChild(note);
