4550ea45f4fdc85c917d6cb735abab7dd9ce520e
MrBurp <andreass.ziegler@gmail.com>
2022-11-17 17:27:34 +0100

    textarea editable

--- script.js
+++ script.js
@@ -16,9 +16,16 @@ function addNewNote(text = "") {
   `;
 
   const deleteBtn = note.querySelector(".delete");
+  const editBtn = note.querySelector(".edit");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector(".textarea");
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
+  });
+
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
   });
 
   document.body.appendChild(note);
